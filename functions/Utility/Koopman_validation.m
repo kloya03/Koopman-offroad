05e42fcd669c8492a88a9c870458505b7d652b9f
Kartik Palmetto Scratch <kloya@clemson.edu>
2025-10-26 19:23:08 -0400

    ....

--- functions/Utility/Koopman_validation.m
+++ functions/Utility/Koopman_validation.m
@@ -1,11 +1,12 @@
 %% 9. Validation Function
 
-function [err_val] = Koopman_validation(valData,MDL_fitr, K,B,C,K_obs)
+function [err_val] = Koopman_validation(Data,MDL_fitr, K,B,C,K_obs)
+% error_val : No. of observation x no. of data samples (trajectories)
 
-ntr = size(valData,4);
+ntr = size(Data,4);
 
 for i=1:ntr
-    [Ymean,Ycov,pos,error_vel,error_pos] = K_RSSID_prediction(valData(:,:,:,i),MDL_fitr,K,B,C,K_obs);
+    [Ymean,Ycov,pos,error_vel,error_pos] = K_RSSID_prediction(Data(:,:,:,i),MDL_fitr,K,B,C,K_obs);
     
     err_val(:,i) = [error_vel;error_pos];
 end
